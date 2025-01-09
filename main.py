8d22cfdca8149719a85818561976b3551787452c
KraivenR <ribeirokraiven@gmail.com>
2025-01-09 12:23:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 class Library:
-  def __init__(self, name, address):
-      self.name = name
-      self.address = address
-      self.books = []
-
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.books = []
+      
     def add_book(self, book):
-        s
+        self.books.append(book)
+        print(f"Book '{book.title}' added successfully to the library.")
+   
+  def display_books(self):
+        if not self.books:
+            print("No books available in the library.")
+            return
+        for book in self.books:
+            print(
+                f"Title: {book.title}, Author: {book.author}, "
+                f"ID: {book.id_number}, Category: {book.category}, "
+                f"Price: {book.get_price()} PLN"
+            )
+class Book:
+    def __init__(self, title, author, id_number, category, price):
+        self.title = title
+        self.author = author
+        self.id_number = id_number
+        self.category = category
+        self.__price = price
+   
+  def get_price(self):
+        return self.__price
+library = Library("Centrum Library", "Zlote Tarasy")
+book1 = Book("The Tempest", "William Shakespeare", "1", "Classic", 120)
+book2 = Book("The Farm", "Joanne Ramos", "2", "Fiction", 150)
+book3 = Book("Born a Crime", "Trevor Noah", "3", "Biography", 110)
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
+library.add_book(book1)
+library.display_books()
     
