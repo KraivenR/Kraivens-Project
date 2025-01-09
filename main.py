c2c7498dbeb19cb9fb2a22c92aa5093f3361f88a
KraivenR <ribeirokraiven@gmail.com>
2025-01-09 12:30:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class Book:
         return self.__price
 library = Library("Pałac Kultury Library", "Zlote Tarasy")
 book1 = Book("The Tempest", "William Shakespeare", "1", "Classic", 120)
-book2 = Book("The Farm", "Joanne Ramos", "2", "Fiction", 150)
+book2 = Book("The Farm", "George Orwell", "2", "Fiction", 150)
 book3 = Book("Born a Crime", "Trevor Noah", "3", "Biography", 110)
 library.add_book(book1)
 library.add_book(book2)
