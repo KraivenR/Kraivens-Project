2840b92162b980aeafa6628a9df90772e440febf
KraivenR <ribeirokraiven@gmail.com>
2025-01-09 12:17:35 +0000

    Add my main.py file with a print fucntion

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 class Library:
-    pass
+    def __init__(self, name,address):
+        self.name = name
+        self.address = address
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def display_books(self):
+        for book in self.books:
+            print(f"Title: {book.title}, Author: {book.author}, "
+                  f"ID: {book.id_number}, Category: {book.category}, "
+                  f"Price: {book.get_price()}pln")
 
 class Book:
-    pass
+    def __init__(self, title, author, id_number, category, price):
+        self.title = title
+        self.author = author
+        self.id_number = id_number
+        self.category = category
+        self.__price = price
+
+    def get_price(self):
+        return self.__price
+
+library = Library("Centrum Library", "Zlote Tarasy")
+book1 = Book("The Tempest", "William Shakespeare", "1", "Classic", 120)
+book2 = Book("The Farm", "Joanne Ramos", "2", "Fiction", 150)
+book3 = Book("Born a Crime", "Trevor Noah", "3", "Biography", 110)
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
+
+library.display_books()
