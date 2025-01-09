2b5ff10614a1da2ab48b9dcf773f4a2a2175feb9
KraivenR <ribeirokraiven@gmail.com>
2025-01-09 12:12:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello from the Library project')
+class Library:
+  def __init__(self, name, address):
+      self.name = name
+      self.address = address
+      self.books = []
+
+    def add_book(self, book):
+        s
+    
