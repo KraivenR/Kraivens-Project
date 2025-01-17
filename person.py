8ab8d6e8f41dc79fbe2aeff90e7d283ed00dd747
KraivenR <ribeirokraiven@gmail.com>
2025-01-17 13:58:28 +0000

    save

--- person.py
+++ person.py
@@ -1,8 +1,11 @@
-class Person:
+from abc import ABC,abstractmethod
+
+class Person(ABC):
+
     def __init__(self, fullname, age, user_id):
         self.fullname = fullname
         self.age = age 
         self.user_id = user_id
-
-def __str__(self): 
-    return f"Name: {self.name}, Age: {self.age}"
+@abstractmethod
+def display_details(self): 
+    return f"Name: {self.fullname}, Age: {self.age}"
