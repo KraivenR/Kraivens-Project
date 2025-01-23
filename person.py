a422c5dd58f1fefcad83bce96385bdb11ebd387c
KraivenR <ribeirokraiven@gmail.com>
2025-01-23 11:31:28 +0000

    save the file

--- person.py
+++ person.py
@@ -1,11 +1,21 @@
-from abc import ABC,abstractmethod
+from abc import ABC, abstractmethod
 
 class Person(ABC):
+    @abstractmethod
 
-    def __init__(self, fullname, age, user_id):
-        self.fullname = fullname
-        self.age = age 
+    def display_details(self):
+        pass
+class Student(Person):
+    def __init__(self,name, surname, age, user_id):
+        self.name = name
+        self.surname = surname
+        self.age = age
         self.user_id = user_id
-@abstractmethod
-def display_details(self): 
-    return f"Name: {self.fullname}, Age: {self.age}"
+
+    def display_details(self):
+        print(f"student full name is {self.name},he is {self.age} years old an his student ID is {self.user_id}")
+
+people = [Student("Kraiven","Ribeiro", 20, "34714")]
+
+for person in people:
+    print(person.display_details())
